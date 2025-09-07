8412cd6fa9f242b2c0cadbc3f2ac2780ff40cec8
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2025-09-08 02:57:24 +0530

    add initial c pattern programs and vscode config.

---
+++ Pattern_solving_Q1.c
@@ -0,0 +1,23 @@
+/*
+Print this Pattern:
+
+* * * *
+* * * * 
+* * * * 
+* * * * 
+
+*/
+#include<stdio.h>
+
+int main(){
+    int i,j;
+
+    for (i=0;i<4;i++) {
+        for(j=0;j<4;j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
